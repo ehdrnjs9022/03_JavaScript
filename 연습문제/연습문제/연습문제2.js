bd881d1690e29759482a93f124338270e70ada7b
ehdrnjs9022 <ehdrnjs9022@naver.com>
2025-01-14 17:31:47 +0900

    window,스톱워치

--- 연습문제/연습문제/연습문제2.js
+++ 연습문제/연습문제/연습문제2.js
@@ -1,14 +1,31 @@
 /* 합계 */
-const number = document.querySelectorAll(".number");
-number.addEventListener("click", () => {
-    if(let num=0;  num > number.length; num++)
+const numbers = document.querySelectorAll(".number");
+const result = document.querySelector("#result"); 
+const reset = document.querySelector("#reset"); 
+ 
 
-
-
-
+  // 향상된 for문 
+  for(let num of numbers){
+    num.addEventListener("click", (e) =>{
+      
+      if(result.innerText.length >= 10);{
+        alert("10자 까지만 입력할 수 있습니다");
+        return;
+      }
+      //textContent == innerText
+      // e.target : 이벤트가 발생된 요소
+      result.innerText += e.target.innerText; // 버튼 내용 누적
     });
+    }
+    /*초기화버튼 */
+    reset.addEventListener("click", ()=>{
+    result.innertext = ""; // 누적된 번호 모두 삭제
+    });
+
+
     
-      alert(`10`)
+    
+
     
     
     
@@ -16,14 +33,3 @@ number.addEventListener("click", () => {
 
 
 
-
-
-/* 초기화 */
-const btn = document.querySelector("#btn");
-btn.addEventListener("click", () => {
-
-
-
-
-
-});
